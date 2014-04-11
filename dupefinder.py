4dceb08dd2637c70a4440a5ca4d5d2c5eb8e3cda
Joshua King <jking_ok@yahoo.com.au>
2014-04-11 23:45:22 +0800

    Python 3 compatibility and more printing
    Runs on Python 3.4 for Windows. Probably broken on Python 2 again
    though. Cleaned up the list of duplicates at the bottom.

--- dupefinder.py
+++ dupefinder.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 
+from os import walk
 import os.path
 import stat
 
@@ -27,7 +28,7 @@ def walker(arg, dirname, filenames):
 # split the given list into a list of lists where the items are distinct
 def separate(items):
     names = {}
-    for k, v in l.iteritems():
+    for k, v in l.items():
         filename = v['name'] + v['ext']
         if filename in names:
             names[filename][k] = v
@@ -36,7 +37,11 @@ def separate(items):
     return names.values()
 
 # main logic
-os.path.walk(".", walker, u"Walker")
+# Old way
+#os.path.walk(".", walker, u"Walker")
+# New way
+for root, dirs, names in os.walk("."):
+    walker(u"Walker", root, names)
 
 # progressively whittle down the list
 # initial assumption: everything is the same
@@ -54,7 +59,11 @@ whittled = [ x for x in nextw if len(x) > 1 ]
 #    print x
 
 # or a summary
-print "From", len(files), "file(s) scanned:",
+print("From", len(files), "file(s) scanned:")
 n = sum([ len(x) for x in whittled ])
-print n, "duplicate files in", len(whittled), "set(s)",
-print len(files) - n, "or", 100 - float(n) / len(files) * 100, "% were unique." 
+print(n, "duplicate files in", len(whittled), "set(s)")
+print(len(files) - n, "or", 100 - float(n) / len(files) * 100, "% were unique.")
+if n > 0:
+    print("Duplicates:")
+    for i, f in enumerate(whittled):
+        print(i + 1, list(f.keys()))
